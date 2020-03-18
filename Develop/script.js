7e2bbbe908694b4b7aa5dd5a88e356e17de40afa
Joy Chen <joychen5069@gmail.com>
2020-03-18 15:51:42 -0400

    added current date

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+//You need to display the current day
+var currentDay = moment().format('LLL');
+console.log(currentDay)
+$("#currentDay").append(currentDay);
+
+//need to create 9 hour schedule
+//need to gray out hours that have already passed
+//need to highlight the current hour
+//need to color upcoming hours
+//when you click the save button, it saves the text to the local storage displays text
+
