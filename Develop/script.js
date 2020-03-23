1d5b95165ce594b1a908c136f4c826b9f125bb2b
Joy Chen <joychen5069@gmail.com>
2020-03-22 22:21:38 -0400

    fixed highlighting part

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-   
 
     //when you load the page, show current date
     var currentDay = moment().format('LLL');
@@ -10,8 +9,12 @@ $(document).ready(function () {
     //create div to put things in
     var planDiv = $('#planContainer');
 
-   
+
     //schedule can only be between 9am-5pm so 0900 to 1700
+    var hourMil = moment().format('HH'); //current miltary time
+    
+    hourMil = 12
+    console.log(hourMil)
     var hours = ""
     for (var i = 9; i < 18; i++) {
         // hours.push(i);
@@ -38,10 +41,13 @@ $(document).ready(function () {
 
         //build rows
         var rows = $("<tr></tr>");
+        rows.attr('id', index)
         // rows.addClass('row');
 
         //build time column
         var hourCol = $("<td></td>");
+        hourCol.attr('class', 'hoursBox');
+        hourCol.attr('id', 'index')
         // hourCol.addClass('hours');
 
         //put time in hours spot
@@ -58,6 +64,18 @@ $(document).ready(function () {
         hourCol.append(hourBox.text());
         // $(rows).append(hourCol);
         console.log(hourCol.text())
+
+        console.log(index)
+        if (i < hourMil) {
+            $(hourCol).attr('class', 'past');
+            console.log('this is in the past')
+        } //if time displayed is greater than current hour, highlight in green
+        else if (i> hourMil) {
+            $(hourCol).attr('class', 'future');
+        } //if time is within current hour, highlight
+        else {
+            $(hourCol).attr('class', 'present');
+        }
 
         // $(rows).append(hourBox.text());
 
@@ -88,7 +106,7 @@ $(document).ready(function () {
         saveCol.append(saveDiv);
 
         //append everything to the row
-        rows.append(hours, notesCol, saveDiv);
+        rows.append(hourCol, notesCol, saveDiv);
         // console.log(rows)
         $('.table').append(rows);
         // $('#planContainer').append($('.table'))
@@ -98,44 +116,40 @@ $(document).ready(function () {
 
 
 
-        //FORMAT TIME BLOCKS COLORS
-        //if time displayed is less than current hour, gray out
-        // function rowColor(hourBox, hours) {
-        //     if (hours < hour24) {
-        //         $(hourBox).attr('class', 'past');
-        //     } //if time displayed is greater than current hour, highlight in green
-        //     else if (hours > hour24) {
-        //         $(hourBox).attr('class', 'future');
-        //     } //if time is within current hour, highlight
-        //     else {
-        //         $(hourBox).attr('class', 'present');
+        // //FORMAT TIME BLOCKS COLORS
+        // // if time displayed is less than current hour, gray out
+        // function rowColor(hours) {
+        //     for (var i = 9; i < 18; i++) {
+
         //     }
+        //     console.log(hours)
+
         // }
 
         //when you click the save button, it saves the text to the local storage displays text
 
 
 
+
+        getStored();
+        //save buttons--find out how to shorten this
+        // console.log(disHour)
+        // console.log(this.toString(disHour))
+        $('#saveid-' + disHour).on("click", function () {
+            console.log("click " + this.disHour);
+            var planindex = $('#input-' + index).val();
+            // var toString = JSON.stringify(plan9)
+            // localStorage.setItem('plan9', plan9)
+
+        })
+        //get information from local storage 
+        function getStored() {
+            var storedPl = localStorage.getItem('plan9')
+            // console.log(storedPl)
+            // document.getElementById("input-9").textContent = storedPl;
+            $('#input-9').val(storedPl);
+        }
     }
-    getStored()
-    //save buttons--find out how to shorten this
+    // rowColor(hourCol, hours)
+})
 
-    $('#saveid-9').on("click", function () {
-        console.log("click9");
-        var plan9 = $('#input-9').val();
-        var toString = JSON.stringify(plan9)
-        localStorage.setItem('plan9', toString)
-
-    })
-
-    function getStored() {
-        var storedPl = localStorage.getItem('plan9')
-        console.log(storedPl)
-        // document.getElementById("input-9").textContent = storedPl;
-        $('#input-9').val(storedPl);
-    }
-
-
-
-    //get information from local storage 
-}) 
