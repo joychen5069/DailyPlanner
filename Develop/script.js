1f036b522d1760880aa35fadb9b42274cca681df
Joy Chen <joychen5069@gmail.com>
2020-03-23 21:59:26 -0400

    cleaned up the JS and CSS. Removed the test times

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,16 @@
 $(document).ready(function () {
 
-
     //when you load the page, show current date
     var currentDay = moment().format('LLL');
     console.log(currentDay)
     $("#currentDay").append(currentDay);
-
-    //create div to put things in
-    var planDiv = $('#planContainer');
-
 
     //schedule can only be between 9am-5pm so 0900 to 1700
     //current miltary time
     var hourMil = moment().format('HH');
 
     //use this to test to make sure code works
-    hourMil = 12
+    // hourMil = 12
     console.log(hourMil)
     var hours = ""
     for (var i = 9; i < 18; i++) {
@@ -39,38 +34,25 @@ $(document).ready(function () {
 
         var index = displayHour
         hours = index + amPM
-        // console.log(hours)
-
+      
         //build rows
         var rows = $("<tr></tr>");
         rows.attr('id', index)
-        // rows.addClass('row');
-
+      
         //build time column
         var hourCol = $("<td></td>");
-        $(hourCol).attr('class', 'time');
-        // hourCol.addClass('time');
-
-
+        hourCol.addClass('time');
+       
         //put time in hours spot
         var hourBox = $("<span></span>");
         hourBox.attr('class', 'time');
         var disHour = displayHour.toString();
 
-        // console.log(disHour, amPM)
-
         //displayHour is working, however, displayHour amPM is not...
         hourBox.text(disHour + amPM);
         // console.log(hourBox.text())
         hourCol.append(hourBox.text());
-        // $(rows).append(hourCol);
-        console.log(hourCol.text())
-
-        console.log(index)
-
-        // $(rows).append(hourBox.text());
-
-        // $(".table").append(rows);
+ 
         //build notes column, needs to be an input section so user can type
         var notesCol = $('<td></td>');
         var dailyNotes = $('<input>');
@@ -92,37 +74,33 @@ $(document).ready(function () {
         saveDiv.attr('class', 'btn saveBtn');
         saveDiv.attr('src', './Assets/save.png')
 
-
         //append saveDiv to saveCol
         saveCol.append(saveDiv);
 
+        //highlight rows depending on if they've past, if its now, or if its in the future
         if (i < hourMil) {
-            $(hourCol).attr('class', 'past');
+            $(hourCol).addClass('past')
+            // $(hourCol).attr('class', 'past');
             $(notesCol).attr('class', 'past');
             console.log('this is in the past')
         } //if time displayed is greater than current hour, highlight in green
         else if (i > hourMil) {
-            $(hourCol).attr('class', 'future');
+            $(hourCol).addClass('future');
             $(notesCol).attr('class', 'future');
         } //if time is within current hour, highlight
         else {
-            $(hourCol).attr('class', 'present');
+            $(hourCol).addClass('present');
             $(notesCol).attr('class', 'present')
         }
-
-
-
         //append everything to the row
         rows.append(hourCol, notesCol, saveDiv);
         // console.log(rows)
         $('.table').append(rows);
-
-
-
     }
 
+    //push notes to localStorage and get them out
     for (let i = 1; i < 13; i++) {
-
+        //for each ID
         getStored();
 
         //try to simplify it all
@@ -148,6 +126,6 @@ $(document).ready(function () {
         }
 
     }
-    // rowColor(hourCol, hours)
+    
 })
 
